d021724be4925e5695931f01e29703304ddd9349
David Arnett <arnettdc@umich.edu>
2021-07-07 18:06:31 -0400

    Blue Square Generator

--- js/main.js
+++ js/main.js
@@ -1,15 +1,61 @@
+var testBox;
+
 function startGame() {
-    myGameArea.start();
+    gameArea.start();
+    testBox = new Rect_Component(30, 30, "red", 140, 120);
+    setUpEvents();
 }
 
-var myGameArea = {
+var gameArea = {
     canvas : document.createElement("canvas"),
+    // runs on page load
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = 1080;
+        this.canvas.height = 560;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
+};
+
+function Rect_Component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    ctx = gameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+}
+
+function generateSquare()
+{
+    var localBox = new Rect_Component(30, 30, "blue",
+        Math.ceil(Math.random() * 1000),
+        Math.ceil(Math.random() * 500));
+}
+
+//events using jquery
+function setUpEvents()
+{
+    var $b_gen = $("#b_Gen");
+    $b_gen.on("mousedown", function(e)
+    {
+        switch (e.which)
+        {
+            //Left click
+            case 1:
+                generateSquare();
+                break;
+            default:
+                break;
+        }
+
+    });
+
+    $b_gen.on("contextmenu", function(e)
+    {
+        return false;
+    });
 }
 
 function eatAss() {
