3c3c1ec58160b1fcb5a2ba0ede34f154bf4c980b
arnettdc <arnettdc@umich.edu>
2021-07-23 01:13:12 -0400

    Bouncing accelerating boys

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ function startGame() {
     //drawImage("images/corn.jpg");
     mainBox = new Rect_Component(30, 30, "red", 140, 120);
     boxes.push(mainBox);
-    secondaryBox = new Rect_Component(450, 40, "blue", 300, 250);
+    secondaryBox = new Rect_Component(40, 40, "blue", 300, 250);
     boxes.push(secondaryBox);
-    boxes.push(new Rect_Component(70, 1000, "purple",
+    boxes.push(new Rect_Component(70, 70, "purple",
         gameArea.canvas.width/2 - 10, gameArea.canvas.height/2 - 10));
     setUpEvents();
 }
@@ -25,8 +25,8 @@ var gameArea = {
         this.FRAMERATE = 120;
         this.frameNo = 0;
         this.interval = setInterval(updateGameArea, (1/this.FRAMERATE)*1000);
+        this.gravity = .25;
     },
-
     clear : function()
     {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -41,7 +41,7 @@ function updateGameArea()
     gameArea.frameNo += 1;
     for (let i = 0; i < boxes.length; ++i)
     {
-        if (i !== 0)
+        if (false)
         {
             if (Math.floor((gameArea.frameNo/gameArea.FRAMERATE) % 2))
             {
@@ -56,6 +56,7 @@ function updateGameArea()
         }
         boxes[i].angle += degToRad(1);
         boxes[i].hasRotated = true;
+        boxes[i].newPos();
         boxes[i].update();
     }
 }
@@ -66,13 +67,18 @@ function Rect_Component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
     this.angle = 0;
+    this.speedX = 5;
+    this.speedY = 0;
     this.x = x;
     this.y = y;
-    // rotateFill() method??? with angle argument. it would handle rotating canvas
+    this.newPos = function()
+    {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    };
     this.update = function()
     {
         let ctx = gameArea.context;
-        // Call rotateFill() here??? or just have it here
         if(this.hasRotated)
         {
             ctx.save();
@@ -88,8 +94,34 @@ function Rect_Component(width, height, color, x, y) {
             ctx.fillStyle = color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
-    }
-
+        if (this.x >= gameArea.canvas.width)
+        {
+            this.x = gameArea.canvas.width;
+            this.speedX = -1*(this.speedX*1.01);
+            if (this.speedX < -1*gameArea.canvas.width+120)
+                this.speedX = -1*gameArea.canvas.width+120;
+            console.log("SpeedX_RIGHT=" + this.speedX);
+        }
+        else if (this.x <= 0)
+        {
+            this.x = 0;
+            //console.log("SpeedX_LEFTBEFORE=" + this.speedX);
+            this.speedX = -1*(this.speedX*1.01);
+            if (this.speedX > gameArea.canvas.width-120)
+                this.speedX = gameArea.canvas.width-120;
+            console.log("SpeedX_LEFT=" + this.speedX);
+        }
+        if (this.y >= gameArea.canvas.height)
+        {
+            //console.log("y: " + this.y + ", height: " + gameArea.canvas.height);
+            this.speedY -= this.speedY*2;
+        }
+        else
+        {
+            //console.log("yeet");
+            this.speedY += gameArea.gravity;
+        }
+    };
 }
 
 function degToRad(degrees)
